9f51c76731804f06aeb8628a1fd6624f7ad79b76
SandeepGamage <sgamage1109@gmail.com>
2025-07-03 22:02:17 +0530

    practice tailwinds css

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,14 @@ function App() {
   return (
     
     <>
-    
-      <div>
-
-
-        <Header/>
-        <ProductCart name="iphone16" price="3000$"/>
-
+    <div className='w-full h-screen bg-pink-200'>
+      <div className='w-[500px] h-[500px] flex flex-col items-center bg-gray-400 relative'>
+        <div className='w-[90px] h-[90px] bg-red-500'></div>
+        <div className='w-[90px] h-[90px] bg-yellow-500 absolute right-[50px] bottom-[50px] z-50'></div>
+        <div className='w-[90px] h-[90px] bg-blue-500 fixed right-[50px] bottom-[50px]'></div>
+        <div className='w-[90px] h-[90px] bg-green-500'></div>
       </div>
-    
+    </div>
     </>
 
   )
--- src/components/product_cart.jsx
+++ src/components/product_cart.jsx
@@ -3,7 +3,7 @@ export default function ProductCart(props){
     console.log(props.name)
 
     return (
-        <div className="h">
+        <div className='bg-green-500'>
             <h1>{props.name}</h1>
             <h1>{props.price}</h1>
         </div>
