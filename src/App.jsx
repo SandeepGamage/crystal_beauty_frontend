954b5dd98a6ff8be0cb1369eaa1b719d7bd2ee5b
SandeepGamage <sgamage1109@gmail.com>
2025-07-03 22:28:30 +0530

    simple changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import ProductCart from './components/product_cart'
 import Header from './components/header'
+import LoginPage from './pages/loginPage'
 
 function App() {
   
   return (
     
     <>
-    <div className='w-full h-screen bg-pink-200'>
+
+ 
+    <LoginPage />
+   
+
+    {/* color box practice(positioning).
+     <div className='w-full h-screen bg-pink-200'>
       <div className='w-[500px] h-[500px] flex flex-col items-center bg-gray-400 relative'>
         <div className='w-[90px] h-[90px] bg-red-500'></div>
         <div className='w-[90px] h-[90px] bg-yellow-500 absolute right-[50px] bottom-[50px] z-50'></div>
         <div className='w-[90px] h-[90px] bg-blue-500 fixed right-[50px] bottom-[50px]'></div>
         <div className='w-[90px] h-[90px] bg-green-500'></div>
       </div>
-    </div>
+    </div> */}
     </>
 
   )
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,10 @@
+// -    not work for loginPage. its must be LoginPage.
+export default function LoginPage(){
+
+    return(
+        <div>
+            <h3>This is Login Page</h3>
+        </div>
+    )
+
+}
